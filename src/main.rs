bcb51cf4e78ae2438ea14aedd23e5578634513c6
Tarik <tariksayshi@gmail.com>
2023-04-26 02:16:14 +0600

    another funcion to generate series

--- src/main.rs
+++ src/main.rs
@@ -21,3 +21,23 @@ fn generate_fibonacci(position: u32) -> u32 {
 
     initial
 }
+
+fn generate_fibonacci_series (how_many: u32) -> Vec<u32> {
+    let mut fibonacci_series: Vec<u32> = vec![0, 1];
+    if how_many < 2 {
+        return fibonacci_series;
+    }
+    
+    let deducted_how_many = how_many - 2;
+    
+    
+    
+    for _number in 1..=deducted_how_many {
+        let (a, b ) = match &fibonacci_series[..] {
+          [.., a, b] => (a, b),
+          _ => panic!("vector need at least two numbers to start the series"),
+        };
+        fibonacci_series.push( a + b );
+    }
+    fibonacci_series
+}
